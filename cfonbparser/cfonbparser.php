5822eb9debb3ee1bfc150568b7ff9d54ad9e9b33
Louis <louis@lacompany.com>
2016-12-11 16:01:45 +0100

    First stable version of CFONB parser

--- cfonbparser/cfonbparser.php
+++ cfonbparser/cfonbparser.php
@@ -35,36 +35,47 @@ class CfonbParser
      * @return array
      */
     public function getCleanData($content) {
+        // Init
         $transactions = array();
+        $content[] =  array();
+        
+        // Loop
         foreach ($content as $raw_line) {
             // Split the line of text every space
-            $line = explode(' ', $raw_line);
-
             // Remove array items containing spaces
+            $line = explode(' ', $raw_line);
             $line = $this->removeEmptyArrayItems($line);
 
-            // Parsing register_code
-            $transaction['register_code'] = substr($line[0], 0, 2);
-
             // Determine what kind of transaction each line is
-            switch ($transaction['register_code']) {
+            $register_code = substr($line[0], 0, 2);
 
-                // Initial balance
+            // Add transaction to array if the line is != transaction details
+            if(isset($transaction) && !empty($transaction) && $register_code != '05'){
+                $transactions[] = $transaction;
+                unset($transaction);
+            }
+
+            switch ($register_code) {
+
+                // Initial and final balance
                 case '01':
+                case '07':
                     $transaction = array();
-                    $transaction['date'] = $line[3];
+                    $transaction['register_code']  = $register_code;
+                    $transaction['name']           = 'balance';
+                    $transaction['date']           = $this->convertDate($line[3], 'date'); 
                     $transaction['encoded_amount'] = $line[4];
+                    $transaction['amount']         = $this->convertAmount($line[4], $register_code);
                     break;
 
-                // Transaction item
+                // New transaction item
                 case '04':
+                    $transaction = array();
+                    $transaction['register_code']  = $register_code;
                     $transaction['name']           = $this->retrieveTransac($line, 'name');
                     $transaction['date']           = $this->retrieveTransac($line, 'date');
                     $transaction['encoded_amount'] = $this->retrieveTransac($line, 'amount');
-                    $transaction['amount']         = $this->convertAmount(
-                        $this->retrieveTransac($line, 'amount'),
-                        $transaction['register_code']
-                    );
+                    $transaction['amount']         = $this->convertAmount($this->retrieveTransac($line, 'amount'), $register_code);
 
                     if($this->isTransactionDeposit($line)){
                         $transaction['deposit']     = $transaction['amount'];
@@ -73,27 +84,16 @@ class CfonbParser
                         $transaction['deposit']     = 0;
                         $transaction['withdrawal']  = $transaction['amount'];
                     }
-
-                    // print_r($line);
-                    // print_r($transaction); 
-
                     break;
 
                 // Transaction details
                 case '05':
-
+                    // Remove first 3 items, concatenate values
+                    $details_array = array_slice($line, 3);
+                    $details       = implode(' ', $details_array);
+                    $transaction['description'] .= $details.' ';
                     break;
-
-                // Final balance
-                case '07':
-
-                    break;
-
-
             }
-
-            // Add transaction to transactions array
-            $transactions[] = $transaction;
         }
 
         // @todo check bank statement integrity
@@ -193,36 +193,15 @@ class CfonbParser
         // Amount trunk without key
         $trunk = substr($amount, 0, -1);
 
-        switch ($register_code) {
-            // Initial or final balance
-            case '01':
-            case '07':
+        // Conversion table
+        $ctable = array(
+            '{' => 0, '}' => 0,
+            'A' => 1, 'B' => 2, 'C' => 3, 'D' => 4, 'E' => 5, 'F' => 6, 'G' => 7, 'H' => 8, 'I' => 9,
+            'J' => 1, 'K' => 2, 'L' => 3, 'M' => 4, 'N' => 5, 'O' => 6, 'P' => 7, 'Q' => 8, 'R' => 9,
+        );
 
-                break;
-
-            // Transaction item
-            case '04':
-                $ctable = array('{', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R');
-                $converted_amount = $trunk . array_search($key, $ctable);
-                break;
-        }
-
-        /* Corresponding table
-            { 0
-            J 1
-            K 2
-                        C 3 /?
-            L 3 /?
-            M 4 /?
-            N 5 /?
-            O 6
-            P 7
-                        H
-                        G
-            Q 8 /?
-            R 9 /?
-
-        */
+        // Concatenate trunk + last digit
+        $converted_amount = $trunk . $ctable[$key];
         
         return $converted_amount;
     }
